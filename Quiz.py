2e8ba7c51c28a05905180abd0638f76277c250c9
ZaahidP <19zparker@salesian.surrey.sch.uk>
2025-05-14 13:56:10 +0100

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from quiz_widgets import QuizLabels, Option_Button
+from quiz_widgets import QuizLabels, Option_Button. QuestionButtons
 
 class quiz_grid(tk.Tk):
     def __init__(self):
@@ -113,20 +113,20 @@ class quiz_grid(tk.Tk):
         self.answer = tk.Button(self.bottom_frame, text="Submit")
         self.answer.grid(column=1, row=1, sticky="nsew")
 
-        self.ques_1 = tk.Button(self.top_frame, text="Question 1",
-                                command=self.question_1)
+        self.ques_1 = QuestionButtons(self.top_frame, "Question 1",
+                                self.question_1)
         self.ques_1.grid(column=1, row=1, sticky="nsew")
 
-        self.ques_2 = tk.Button(self.top_frame, text="Question 2",
-                                command=self.question_2)
+        self.ques_2 = QuestionButtons(self.top_frame, "Question 2",
+                                self.question_2)
         self.ques_2.grid(column=2, row=1, sticky="nsew")
 
-        self.ques_3 = tk.Button(self.top_frame, text="Question 3",
-                                command=self.question_3)
+        self.ques_3 = QuestionButtons(self.top_frame, "Question 3",
+                                self.question_3)
         self.ques_3.grid(column=1, row=2, sticky="nsew")
 
-        self.ques_4 = tk.Button(self.top_frame, text="Question 4",
-                                command=self.question_4)
+        self.ques_4 = QuestionButtons(self.top_frame, "Question 4",
+                                self.question_4)
         self.ques_4.grid(column=2, row=2, sticky="nsew")
 
     # Question buttons functionalities
