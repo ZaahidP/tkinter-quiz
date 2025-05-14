58cee17cb4dc57fd49f8cce01a2aea34634b3bd8
ZaahidP <19zparker@salesian.surrey.sch.uk>
2025-05-14 14:28:03 +0100

    Update quiz_events.py

--- quiz_events.py
+++ quiz_events.py
@@ -10,29 +10,29 @@ def question_1(question, btn1, btn2, btn3, btn4):
         self.question_selected = "one"
 
     def question_2(question, btn1, btn2, btn3, btn4):
-        self.question.configure(text="Question 2")
-        self.btn1.configure(text="q2")
-        self.btn2.configure(text="q2")
-        self.btn3.configure(text="q2")
-        self.btn4.configure(text="q2")
+        question.configure(text="Question 2")
+        btn1.configure(text="q2")
+        btn2.configure(text="q2")
+        btn3.configure(text="q2")
+        btn4.configure(text="q2")
 
         self.question_selected = "two"
 
     def question_3(question, btn1, btn2, btn3, btn4):
-        self.question.configure(text="Question 3")
-        self.btn1.configure(text="q3")
-        self.btn2.configure(text="q3")
-        self.btn3.configure(text="q3")
-        self.btn4.configure(text="q3")
+        question.configure(text="Question 3")
+        btn1.configure(text="q3")
+        btn2.configure(text="q3")
+        btn3.configure(text="q3")
+        btn4.configure(text="q3")
 
         self.question_selected = "three"
 
     def question_4(question, btn1, btn2, btn3, btn4):
-        self.question.configure(text="Question 4")
-        self.btn1.configure(text="q4")
-        self.btn2.configure(text="q4")
-        self.btn3.configure(text="q4")
-        self.btn4.configure(text="q4")
+        question.configure(text="Question 4")
+        btn1.configure(text="q4")
+        btn2.configure(text="q4")
+        btn3.configure(text="q4")
+        btn4.configure(text="q4")
 
         self.question_selected = "four"
 
