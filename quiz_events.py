daefb5dc88765de916f9d445aa9bc081d747767c
ZaahidP <19zparker@salesian.surrey.sch.uk>
2025-05-14 14:17:41 +0100

    Update quiz_events.py

--- quiz_events.py
+++ quiz_events.py
@@ -1,15 +1,15 @@
 import tkinter as tk
 
-def question_1(self):
-        self.question.configure(text="Question 1")
-        self.btn1.configure(text="q1")
-        self.btn2.configure(text="q1")
-        self.btn3.configure(text="q1")
-        self.btn4.configure(text="q1")
+def question_1(question, btn1, btn2, btn3, btn4):
+        question.configure(text="Question 1")
+        btn1.configure(text="q1")
+        btn2.configure(text="q1")
+        btn3.configure(text="q1")
+        btn4.configure(text="q1")
 
         self.question_selected = "one"
 
-    def question_2(self):
+    def question_2(question, btn1, btn2, btn3, btn4):
         self.question.configure(text="Question 2")
         self.btn1.configure(text="q2")
         self.btn2.configure(text="q2")
@@ -18,7 +18,7 @@ def question_1(self):
 
         self.question_selected = "two"
 
-    def question_3(self):
+    def question_3(question, btn1, btn2, btn3, btn4):
         self.question.configure(text="Question 3")
         self.btn1.configure(text="q3")
         self.btn2.configure(text="q3")
@@ -27,7 +27,7 @@ def question_1(self):
 
         self.question_selected = "three"
 
-    def question_4(self):
+    def question_4(question, btn1, btn2, btn3, btn4):
         self.question.configure(text="Question 4")
         self.btn1.configure(text="q4")
         self.btn2.configure(text="q4")
