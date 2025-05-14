32168d123c052b3b95e4e924a0dc2f52c951c4e9
ZaahidP <19zparker@salesian.surrey.sch.uk>
2025-05-14 14:28:38 +0100

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -131,45 +131,6 @@ class quiz_grid(tk.Tk):
         self.ques_4.configure(command=lambda: question_4(self.question, self.btn1, self.btn2, self.btn3, self.btn4)
         self.ques_4.grid(column=2, row=2, sticky="nsew")
 
-    # Question buttons functionalities
-    
-    def question_1(self):
-        self.question.configure(text="Question 1")
-        self.btn1.configure(text="q1")
-        self.btn2.configure(text="q1")
-        self.btn3.configure(text="q1")
-        self.btn4.configure(text="q1")
-
-        self.question_selected = "one"
-
-    def question_2(self):
-        self.question.configure(text="Question 2")
-        self.btn1.configure(text="q2")
-        self.btn2.configure(text="q2")
-        self.btn3.configure(text="q2")
-        self.btn4.configure(text="q2")
-
-        self.question_selected = "two"
-
-    def question_3(self):
-        self.question.configure(text="Question 3")
-        self.btn1.configure(text="q3")
-        self.btn2.configure(text="q3")
-        self.btn3.configure(text="q3")
-        self.btn4.configure(text="q3")
-
-        self.question_selected = "three"
-
-    def question_4(self):
-        self.question.configure(text="Question 4")
-        self.btn1.configure(text="q4")
-        self.btn2.configure(text="q4")
-        self.btn3.configure(text="q4")
-        self.btn4.configure(text="q4")
-
-        self.question_selected = "four"
-
-
     # Option button functionalities
 
     def option_func(self):
