9eb7a7739b29d67e7b66a4f34572ab3b69dddd6a
ZaahidP <19zparker@salesian.surrey.sch.uk>
2025-05-14 14:58:31 +0100

    Update quiz_events.py

--- quiz_events.py
+++ quiz_events.py
@@ -8,8 +8,8 @@ def question_1(question, btn1, btn2, btn3, btn4):
         btn4.configure(text="q1")
 
         self.question_selected = "one"
-
-    def question_2(question, btn1, btn2, btn3, btn4):
+        
+def question_2(question, btn1, btn2, btn3, btn4):
         question.configure(text="Question 2")
         btn1.configure(text="q2")
         btn2.configure(text="q2")
@@ -18,7 +18,7 @@ def question_1(question, btn1, btn2, btn3, btn4):
 
         self.question_selected = "two"
 
-    def question_3(question, btn1, btn2, btn3, btn4):
+def question_3(question, btn1, btn2, btn3, btn4):
         question.configure(text="Question 3")
         btn1.configure(text="q3")
         btn2.configure(text="q3")
@@ -26,8 +26,9 @@ def question_1(question, btn1, btn2, btn3, btn4):
         btn4.configure(text="q3")
 
         self.question_selected = "three"
+        
 
-    def question_4(question, btn1, btn2, btn3, btn4):
+def question_4(question, btn1, btn2, btn3, btn4):
         question.configure(text="Question 4")
         btn1.configure(text="q4")
         btn2.configure(text="q4")
