6058dc56bec4dc509503e62d3fce4c6d5618060d
ZaahidP <19zparker@salesian.surrey.sch.uk>
2025-05-14 13:40:12 +0100

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -91,17 +91,17 @@ class quiz_grid(tk.Tk):
                               self.option_1)
         self.btn1.grid(row=1, column=1)
         
-        self.btn2 = tk.Button(self.frame2,
-                             text="Option 3")
-        self.btn2.grid(row=1, column=1, sticky="nsew")
+        self.btn2 = Option_Button(self.frame2,
+                             "Option 3")
+        self.btn2.grid(row=1, column=1)
 
-        self.btn3 = tk.Button(self.frame3,
-                              text="Option 2")
-        self.btn3.grid(row=1, column=1, sticky="nsew")
+        self.btn3 = Option_Button(self.frame3,
+                              "Option 2")
+        self.btn3.grid(row=1, column=1)
         
-        self.btn4 = tk.Button(self.frame4,
-                              text="Option 4")
-        self.btn4.grid(row=1, column=1, sticky="nsew")
+        self.btn4 = Option_Button(self.frame4,
+                              "Option 4")
+        self.btn4.grid(row=1, column=1)
 
         self.question = QuizLabels(self.mid_frame, "Select a question to begin!")
         self.question.grid(column=0, row=0)
