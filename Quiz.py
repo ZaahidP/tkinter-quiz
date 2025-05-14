02286d9186708e038e5b0482c7c161bc9fb725bb
ZaahidP <19zparker@salesian.surrey.sch.uk>
2025-05-14 13:56:34 +0100

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from quiz_widgets import QuizLabels, Option_Button. QuestionButtons
+from quiz_widgets import QuizLabels, Option_Button, QuestionButtons
 
 class quiz_grid(tk.Tk):
     def __init__(self):
